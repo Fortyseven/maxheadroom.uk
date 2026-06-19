5c7e23c06ddcbd8667e10f36597a086ae8d9f2a3
Fortyseven <fortyseven@network47.org>
2026-06-19 02:19:48 -0400

    feat: updates site name

--- site/src/_data/meta.js
+++ site/src/_data/meta.js
@@ -2,6 +2,6 @@ module.exports = {
     // NOTE: `process.env.URL` is provided by Netlify, and may need
     // adjusted pending your host
     url: process.env.URL || "http://localhost:8080",
-    siteName: "The Max Headroom Chronicles Revival",
+    siteName: "MaxHeadroom.uk",
     siteDescription: "A fan-sourced rebuild of MaxHeadroom.com.",
 };
